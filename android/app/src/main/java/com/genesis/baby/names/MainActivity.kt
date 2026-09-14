08a5480c2e1e33113a79561cf717cec7e619bea2
Amal Raj <amalrajacl@gmail.com>
2026-09-14 16:45:18 +0530

    Implement splash screen enhancements for Android and iOS; update MainActivity to handle splash screen exit animations on Android 12+, modify styles.xml to include new splash screen attributes, and adjust LaunchScreen.storyboard for improved background color consistency.

--- android/app/src/main/java/com/genesis/baby/names/MainActivity.kt
+++ android/app/src/main/java/com/genesis/baby/names/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.genesis.baby.names
 
+import android.os.Build
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +14,15 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "MyProject"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      splashScreen.setOnExitAnimationListener { splashView ->
+        splashView.remove()
+      }
+    }
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
